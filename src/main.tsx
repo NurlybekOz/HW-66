be93929c1c73808571f2957a50bee57cd1524336
nurlybek <orozovnurlybek@gmail.com>
2025-02-14 23:56:46 +0600

    init app

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.tsx'
+import {BrowserRouter} from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css'
+import {CssBaseline} from "@mui/material";
+import {ToastContainer} from "react-toastify";
+
+createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+    <CssBaseline/>
+    <ToastContainer />
+    <App />
+    </BrowserRouter>
+
+)
